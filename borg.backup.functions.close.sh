c1f6bb443a55c93eb143022a5a3c79a827a9fc22
Clemens Schwaighofer <clemens.schwaighofer@egplusww.com>
2022-03-31 09:18:23 +0900

    Fix for close with empty parameter

--- borg.backup.functions.close.sh
+++ borg.backup.functions.close.sh
@@ -3,7 +3,7 @@
 # unset borg settings
 unset BORG_BASE_DIR BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK BORG_RELOCATED_REPO_ACCESS_IS_OK
 # error abort without duration and error notice
-if [ $# -ge 1 ] && [ $1 -eq 1 ]; then
+if [ $# -ge 1 ] && [ "$1" -eq 1 ]; then
 	echo "=== [ERROR: $(date +'%F %T')] ==[${MODULE}]====================================>";
 else
 	DURATION=$[ $(date +'%s')-$START ];
