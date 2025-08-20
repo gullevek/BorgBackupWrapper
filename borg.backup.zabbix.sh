de9d5d3588f6a4d9b1686daa89f6fdb25f296b96
Clemens Schwaighofer <clemens.schwaighofer@egplusww.com>
2025-08-20 22:11:38 +0900

    Zabbix backup set port to 5432 default to override internal set ports

--- borg.backup.zabbix.sh
+++ borg.backup.zabbix.sh
@@ -37,6 +37,9 @@ fi;
 if [ "${ZABBIX_DATABASE}" = "psql" ]; then
 	OPT_ZABBIX_DUMP="-C";
 fi;
+if [ -z "${ZABBIX_DB_PORT}" ]; then
+	ZABBIX_DB_PORT="5432";
+fi;
 if [ "${ZABBIX_DATABASE}" != "psql" ] && [ "${ZABBIX_DATABASE}" != "mysql" ]; then
 	echo "[! $(date +'%F %T')] Zabbix dump must have database set to either psql or mysql";
 	. "${DIR}/borg.backup.functions.close.sh" 1;
@@ -76,7 +79,7 @@ if [ ${DEBUG} -eq 1 ] || [ ${DRYRUN} -eq 1 ]; then
 	fi;
 fi;
 if [ ${DRYRUN} -eq 0 ]; then
-	${ZABBIX_DUMP_BIN} -t ${ZABBIX_DATABASE} ${OPT_ZABBIX_UNKNOWN_TABLES} ${OPT_ZABBIX_DUMP} ${OPT_ZABBIX_CONFIG} -o - | ${BORG_CALL};
+	${ZABBIX_DUMP_BIN} -P ${ZABBIX_DB_PORT} -t ${ZABBIX_DATABASE} ${OPT_ZABBIX_UNKNOWN_TABLES} ${OPT_ZABBIX_DUMP} ${OPT_ZABBIX_CONFIG} -o - | ${BORG_CALL};
 fi;
 if [ -z "${ONE_TIME_TAG}" ]; then
 	printf "${PRINTF_SUB_BLOCK}" "PRUNE" "$(date +'%F %T')" "${MODULE}";
